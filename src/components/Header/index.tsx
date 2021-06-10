4e32ca68a159bda8685f6f78d8b20f0eb85cb2a6
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-06-10 14:48:45 -0300

    feat(*): add responsive pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,31 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../context/SidebarDrawerContext";
 import { Logo } from "./Logo";
 import { NotificationNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion: boolean = useBreakpointValue({
     base: false,
     lg: true,
   })
   return (
     <Flex as="header" w="100%" maxW={1480} h="20" mx="auto" mt="4" px="6" align="center">
+
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"></IconButton>
+      )}
+      
       <Logo />
       
       {isWideVersion && <SearchBox />}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,14 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, List, ListItem, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
   return (
     <Box>
       <Header />
@@ -17,37 +21,59 @@ export default function UserList() {
               Criar novo
             </Button>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px="6" color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th w="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gustavo Vieira</Text>
-                    <Text fontSize="sm" color="gray.300">gustavovieira992@gmail.com</Text>
-                  </Box>
-                </Td>
-                <Td>04 de abril de 2021</Td>
-                <Td>
-                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
+          {isWideVersion && (
+            <Table colorScheme="whiteAlpha">
+              <Thead>
+                <Tr>
+                  <Th px="6" color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  <Th>Data de cadastro</Th>
+                  <Th w="8"></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td px="6">
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">Gustavo Vieira</Text>
+                      <Text fontSize="sm" color="gray.300">gustavovieira992@gmail.com</Text>
+                    </Box>
+                  </Td>
+                  <Td>04 de abril de 2021</Td>
+                  <Td>
+                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
+                    Editar
+                  </Button>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          )}
+          {!isWideVersion && (
+            <List spacing={8} w="100%">
+              <ListItem borderBottomColor="gray.700" borderBottomWidth={1} pb={6}>
+                <Text fontWeight="bold">Gustavo Vieira</Text>
+                <Text fontSize="sm" color="gray.300">gustavovieira992@gmail.com</Text>
+                <Text fontSize="sm" color="gray.300">04 de abril de 2021</Text>
+                <Button mt="3" as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
                   Editar
                 </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+              </ListItem>
+              <ListItem borderBottomWidth={1} pb={6} borderBottomColor="gray.700">
+                <Text fontWeight="bold">Gustavo Vieira</Text>
+                <Text fontSize="sm" color="gray.300">gustavovieira992@gmail.com</Text>
+                <Text fontSize="sm" color="gray.300">04 de abril de 2021</Text>
+                <Button mt="3" as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
+                  Editar
+                </Button>
+              </ListItem>
+            </List>
+          )}
           <Pagination />
         </Box>
       </Flex>
