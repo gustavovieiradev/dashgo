c80b33f2a6e0f3296a81f4576d1a5ac2aaae1286
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-06-10 22:32:43 -0300

    feat(*): add react query and list users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,34 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, List, ListItem, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, List, ListItem, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const {data, isLoading, error} = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+    return users;
+  });
+
+  console.log(data);
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -24,60 +47,74 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          {isWideVersion && (
-            <Table colorScheme="whiteAlpha">
-              <Thead>
-                <Tr>
-                  <Th px="6" color="gray.300" width="8">
-                    <Checkbox colorScheme="pink" />
-                  </Th>
-                  <Th>Usuário</Th>
-                  <Th>Data de cadastro</Th>
-                  <Th w="8"></Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td px="6">
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Gustavo Vieira</Text>
-                      <Text fontSize="sm" color="gray.300">gustavovieira992@gmail.com</Text>
-                    </Box>
-                  </Td>
-                  <Td>04 de abril de 2021</Td>
-                  <Td>
-                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
+
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Text justify="center" >Falha ao obter dados dos usuários</Text>
+          ) : isWideVersion ? (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px="6" color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    <Th>Data de cadastro</Th>
+                    <Th w="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px="6">
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                        </Box>
+                      </Td>
+                      <Td>{user.createdAt}</Td>
+                      <Td>
+                      <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
+                        Editar
+                      </Button>
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          ) : (
+            <>
+              <List spacing={8} w="100%">
+                <ListItem borderBottomColor="gray.700" borderBottomWidth={1} pb={6}>
+                  <Text fontWeight="bold">Gustavo Vieira</Text>
+                  <Text fontSize="sm" color="gray.300">gustavovieira992@gmail.com</Text>
+                  <Text fontSize="sm" color="gray.300">04 de abril de 2021</Text>
+                  <Button mt="3" as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
                     Editar
                   </Button>
-                  </Td>
-                </Tr>
-              </Tbody>
-            </Table>
+                </ListItem>
+                <ListItem borderBottomWidth={1} pb={6} borderBottomColor="gray.700">
+                  <Text fontWeight="bold">Gustavo Vieira</Text>
+                  <Text fontSize="sm" color="gray.300">gustavovieira992@gmail.com</Text>
+                  <Text fontSize="sm" color="gray.300">04 de abril de 2021</Text>
+                  <Button mt="3" as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
+                    Editar
+                  </Button>
+                </ListItem>
+              </List>
+              <Pagination />
+            </>
           )}
-          {!isWideVersion && (
-            <List spacing={8} w="100%">
-              <ListItem borderBottomColor="gray.700" borderBottomWidth={1} pb={6}>
-                <Text fontWeight="bold">Gustavo Vieira</Text>
-                <Text fontSize="sm" color="gray.300">gustavovieira992@gmail.com</Text>
-                <Text fontSize="sm" color="gray.300">04 de abril de 2021</Text>
-                <Button mt="3" as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
-                  Editar
-                </Button>
-              </ListItem>
-              <ListItem borderBottomWidth={1} pb={6} borderBottomColor="gray.700">
-                <Text fontWeight="bold">Gustavo Vieira</Text>
-                <Text fontSize="sm" color="gray.300">gustavovieira992@gmail.com</Text>
-                <Text fontSize="sm" color="gray.300">04 de abril de 2021</Text>
-                <Button mt="3" as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="20" /> }>
-                  Editar
-                </Button>
-              </ListItem>
-            </List>
-          )}
-          <Pagination />
+          
         </Box>
       </Flex>
     </Box>
